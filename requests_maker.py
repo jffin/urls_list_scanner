6ddf015c3afb6f7d3f89b2a13f2bd64864f08d25
jffin <yougetthat.f@gmail.com>
2021-02-16 22:31:52 +0200

    rewrote to bounded semaphore with asyncio ensure future 0.0.9

--- requests_maker.py
+++ requests_maker.py
@@ -9,7 +9,7 @@ from typing import Union, Dict, List, Tuple, Any, Type
 import asyncio
 import argparse
 
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, BoundedSemaphore
 from asyncio.exceptions import TimeoutError
 
 from aiohttp import ClientSession, ClientTimeout, InvalidURL, \
@@ -20,10 +20,10 @@ ASYNCIO_GATHER_TYPE: Type = Tuple[
     Union[BaseException, Any], Union[BaseException, Any], Union[BaseException, Any]
 ]
 
-REQUESTS_LIMIT: int = 100
+SIMULTANEOUS_CONCURRENT_TASKS: int = 51
 
 USER_AGENT: str = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)' \
-             ' Chrome/85.0.4183.102 Safari/537.36'
+                  ' Chrome/85.0.4183.102 Safari/537.36'
 
 RESULT_FILE_NAME: str = 'result.json'
 
@@ -45,34 +45,38 @@ class RequestManager:
         obj: RequestManager = cls(urls=urls, timeout=timeout)
         return await obj.make_request()
 
-    async def _fetch(self, url: URL, session: ClientSession) -> Dict[str, Union[str, int]]:
-        result: Dict[str, str] = {'url': str(url)}
-        try:
-            async with session.get(url, headers=self.headers) as response:
-                content_length: str = '0'
-                if 'content-length' in response.headers:
-                    content_length = response.headers['content-length']
+    async def _fetch(self, url: URL, session: ClientSession, semaphore: BoundedSemaphore) -> Dict[str, Union[str, int]]:
+        async with semaphore:
+            result: Dict[str, str] = {'url': str(url)}
+            try:
+                async with session.get(url, headers=self.headers) as response:
+                    content_length: str = '0'
+                    if 'content-length' in response.headers:
+                        content_length = response.headers['content-length']
+                    result.update({
+                        'status_code': response.status,
+                        'content_length': content_length,
+                        'stream_reader': response.content.total_bytes,
+                        'body_length': len(await response.read()),
+                        'error': '',
+                    })
+            except (ClientConnectorError, ClientResponseError, ServerTimeoutError, TimeoutError, InvalidURL) as e:
                 result.update({
-                    'status_code': response.status,
-                    'content_length': content_length,
-                    'stream_reader': response.content.total_bytes,
-                    'body_length': len(await response.read()),
-                    'error': '',
+                    'status_code': 0,
+                    'content_length': 0, 'stream_reader': 0, 'body_length': 0,
+                    'error': e and str(e) or 'Something Went Wrong'
                 })
-        except (ClientConnectorError, ClientResponseError, ServerTimeoutError, TimeoutError, InvalidURL) as e:
-            result.update({
-                'status_code': 0,
-                'content_length': 0, 'stream_reader': 0, 'body_length': 0,
-                'error': e and str(e) or 'Something Went Wrong'
-            })
-        finally:
-            return result
+            finally:
+                return result
 
     async def make_request(self) -> ASYNCIO_GATHER_TYPE:
+        asyncio_semaphore = asyncio.BoundedSemaphore(SIMULTANEOUS_CONCURRENT_TASKS)
         async with self.session as session:
-            return await asyncio.gather(
-                *[self._fetch(url=url, session=session) for url in self.urls], return_exceptions=True,
-            )
+            return await asyncio.gather(*[
+                asyncio.ensure_future(
+                    self._fetch(url=url, session=session, semaphore=asyncio_semaphore)
+                ) for url in self.urls
+            ], return_exceptions=True, )
 
     @property
     def urls(self) -> List[URL]:
@@ -91,7 +95,6 @@ async def main() -> None:
     # Construct the argument parser
     ap: argparse.ArgumentParser = argparse.ArgumentParser(description='scan list of urls')
     # Add the arguments to the parser
-    # ap.add_argument('urls', metavar='u', type=str, nargs='+', help='list of urls')
     ap.add_argument(
         '--input', type=pathlib.Path, metavar='PATH', dest='path_to_urls',
         help='Path to file with input. Example: "/wd/input.txt"',
@@ -111,4 +114,5 @@ def write_results_to_file(results: ASYNCIO_GATHER_TYPE) -> None:
 
 if __name__ == '__main__':
     loop: AbstractEventLoop = asyncio.get_event_loop()
+    loop.set_debug(True)
     loop.run_until_complete(main())
